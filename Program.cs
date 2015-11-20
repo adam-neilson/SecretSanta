319bb4ec782daa424037e5d0fe162c552b8c187c
Adam Neilson <aneilson82@gmail.com>
2015-11-20 11:02:13 +1000

    Added support for preventing matches a person with the same receiver as the previous year's run.

--- Program.cs
+++ Program.cs
@@ -30,15 +30,25 @@ Merry Christmas!";
             new Relationship(personA, personD),
         };
 
+        private static IEnumerable<Relationship> PreviousCycleMatches = new List<Relationship>
+        {
+            new Relationship(personA, personC),
+            new Relationship(personB, personA),
+            new Relationship(personC, personD),
+            new Relationship(personD, personB),
+        };
+        
         static void Main(string[] args)
         {
             var masterPossiblities = Persons.ToDictionary(person => person, person =>
-                {
-                    var relationships = Relationships.Where(x => x.Left == person || x.Right == person).Select(relationship => relationship.Left == person ? relationship.Right : relationship.Left);
+            {
+                var relationships = Relationships.Where(x => x.Left == person || x.Right == person).Select(relationship => relationship.Left == person ? relationship.Right : relationship.Left);
+                var previousMatches = PreviousCycleMatches.Where(x => x.Left == person).Select(x => x.Right);
 
-                    return Persons.Except(relationships.Concat(new[] { person }));
-                });
+                return Persons.Except(relationships).Except(previousMatches).Except(new[] { person });
+            });
 
+            retry:
             var giftersTodo = Persons.ToList();
             var receiversTodo = Persons.ToList();
             var matches = new List<Tuple<Person, Person>>();
@@ -51,6 +61,17 @@ Merry Christmas!";
                 var possiblities = receiversTodo.Intersect(masterPossiblities[person]).Where(p => !giftersTodo.Any(g => !receiversTodo.Intersect(masterPossiblities[g]).Except(new[] { p }).Any())).ToList();
 
                 var count = possiblities.Count();
+
+                // TODO: Known flaws with this logic. It's possible for the match resolution to crash if the only receivers left conflict for two remaining gifters
+                // Probably needs a rewrite. For time being just have a goto here if the resolution process encounters this problem, i.e. start over.
+                if (count == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Could not resolve receiver for gifter {person.Name}. Retrying entire pool.");
+                    Console.ResetColor();
+                    goto retry;
+                }
+
                 var value = Randomiser.Next(count); // Note, less than the specified maximum, so count is fine.
 
                 var match = possiblities.ElementAt(value);
@@ -67,14 +88,14 @@ Merry Christmas!";
                 // Paranoid assertion
                 if (match.Item1 == match.Item2 || Relationships.Any(x => new[] { x.Left, x.Right }.SequenceEqual(new[] { match.Item1, match.Item2 })))
                 {
-                    throw new Exception(string.Format("Invalid match {0} - {1}!", match.Item1.Name, match.Item2.Name));
+                    throw new Exception($"Invalid match {match.Item1.Name} - {match.Item2.Name}!");
                 }
 
-                Console.WriteLine("Sending email to {0}", match.Item1.Email);
+                Console.WriteLine($"Sending email to {match.Item1.Email}");
                 smtpClient.Send("sender@gmail.com", match.Item1.Email, "Your Secret Santa Recipient!", string.Format(EmailBodyFormat, match.Item1.Name, match.Item2.Name));
             }
 
-            string summary = string.Join("\r\n", matches.Select(match => string.Format("{0} is buying for {1}", match.Item1, match.Item2)));
+            string summary = string.Join("\r\n", matches.Select(match => $"{match.Item1} is buying for {match.Item2}"));
             smtpClient.Send("sender@gmail.com", Auditor, "Secret Santa Outcome", summary);
         }
     }
